d5911fbb635a2509a4409b573a261a6dd7333c97
Thomas Vogl <thomas.vogl@respon.se>
2023-10-20 17:14:13 +0200

    ....

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
-use std::sync::Mutex;
 use crate::xwf::example::ExampleLib;
 
 pub mod xwf;
 
-//needed_use_declarations!();
+needed_use_declarations!();
 
-//export_all_functions!(ExampleLib);
+export_all_functions!(EXAMPLE_LIB, ExampleLib);
 
 
 
--- src/xwf/api/macros.rs
+++ src/xwf/api/macros.rs
@@ -6,9 +6,10 @@ macro_rules! export_xt_init {
     ($variable:ident) => {
         #[no_mangle]
         #[allow(non_snake_case, unused_variables)]
-        pub unsafe extern "C"  fn XT_Init(nVersion: DWORD, nFlags: DWORD, hMainWnd: HANDLE, lpReserved: PVOID) -> LONG {
+        pub extern "C"  fn XT_Init(nVersion: DWORD, nFlags: DWORD, hMainWnd: HANDLE, lpReserved: PVOID) -> LONG {
             *RAW_API.lock().unwrap() = Some(RawApi::load_no_error());
             let logger = WriteLogger::init(LevelFilter::Debug, Config::default(), Application::new());
+
             let xtension_version = $variable.lock().unwrap().xtension_version();
             info!("X-Tension {}, Version {}.{}.{}", $variable.lock().unwrap().xtension_name(), xtension_version.0, xtension_version.1, xtension_version.2 );
             debug!("XT_Init called");
@@ -22,7 +23,7 @@ macro_rules! export_xt_done {
     ($variable:ident) => {
         #[no_mangle]
         #[allow(non_snake_case, unused_variables)]
-        pub unsafe extern "C" fn XT_Done(lpReserved: PVOID)
+        pub extern "C" fn XT_Done(lpReserved: PVOID)
             -> LONG {
             debug!("XT_Done called");
             $variable.lock().unwrap().xt_done();
@@ -36,7 +37,7 @@ macro_rules! export_xt_about {
     ($variable:ident) => {
         #[no_mangle]
         #[allow(non_snake_case, unused_variables)]
-        pub unsafe extern "C" fn XT_About(hParentWnd: HANDLE, lpReserved: PVOID)
+        pub extern "C" fn XT_About(hParentWnd: HANDLE, lpReserved: PVOID)
             -> LONG {
             debug!("XT_About called");
             $variable.lock().unwrap().xt_about(Window::new(hParentWnd));
@@ -49,7 +50,7 @@ macro_rules! export_xt_prepare {
     ($variable:ident) => {
         #[no_mangle]
         #[allow(non_snake_case, unused_variables)]
-        pub unsafe extern "C" fn XT_Prepare(hVolume: HANDLE, hEvidence: HANDLE,  nOpType: DWORD, lpReserved: PVOID
+        pub extern "C" fn XT_Prepare(hVolume: HANDLE, hEvidence: HANDLE,  nOpType: DWORD, lpReserved: PVOID
         ) -> LONG {
             debug!("XT_Prepare called");
 
@@ -72,7 +73,7 @@ macro_rules! export_xt_finalize {
     ($variable:ident) => {
         #[no_mangle]
         #[allow(non_snake_case, unused_variables)]
-        pub unsafe extern "C" fn XT_Finalize(hVolume: HANDLE, hEvidence: HANDLE,  nOpType: DWORD, lpReserved: PVOID
+        pub extern "C" fn XT_Finalize(hVolume: HANDLE, hEvidence: HANDLE,  nOpType: DWORD, lpReserved: PVOID
         ) -> LONG {
             debug!("XT_Finalize called");
             let opt_op_type = XtPrepareOpType::try_from(nOpType);
@@ -93,7 +94,7 @@ macro_rules! export_xt_process_item {
     ($variable:ident) => {
         #[no_mangle]
         #[allow(non_snake_case, unused_variables)]
-        pub unsafe extern "C" fn XT_ProcessItem(nItemID: LONG,  lpReserved: PVOID) -> LONG {
+        pub extern "C" fn XT_ProcessItem(nItemID: LONG,  lpReserved: PVOID) -> LONG {
             let item = Item::new(nItemID);
 
             $variable.lock().unwrap().xt_process_item(item).into()
@@ -105,7 +106,7 @@ macro_rules! export_process_item_ex {
     ($variable:ident) => {
         #[no_mangle]
         #[allow(non_snake_case, unused_variables)]
-        pub unsafe extern "C" fn XT_ProcessItemEx(nItemID: LONG, hItem: HANDLE,  lpReserved: PVOID) -> LONG {
+        pub extern "C" fn XT_ProcessItemEx(nItemID: LONG, hItem: HANDLE,  lpReserved: PVOID) -> LONG {
             debug!("XT_ProcessItemEx called");
             let res_item = ItemHandle::new(hItem, Item::new(nItemID));
             if res_item.is_err() {
@@ -129,15 +130,24 @@ macro_rules! export_all_functions_ex {
 }
 
 #[macro_export]
+macro_rules! create_static_var {
+    ($variable_name:ident, $variable_type:ty) => {
+        static $variable_name:Mutex<$variable_type> = Mutex::new(<$variable_type>::create());
+    }
+}
+
+#[macro_export]
 macro_rules! export_all_functions {
-    ($libtype:ty) => {
-        static mylib:Mutex<$libtype> = Mutex::new(<$libtype>::create());
-        export_xt_init!(mylib);
-        export_xt_prepare!(mylib);
-        export_xt_finalize!(mylib);
-        export_xt_done!(mylib);
-        export_xt_about!(mylib);
-        export_xt_process_item!(mylib);
+    ($variable_name:ident, $variable_type:ty) => {
+
+        create_static_var!($variable_name, $variable_type);
+
+        export_xt_init!($variable_name);
+        export_xt_prepare!($variable_name);
+        export_xt_finalize!($variable_name);
+        export_xt_done!($variable_name);
+        export_xt_about!($variable_name);
+        export_xt_process_item!($variable_name);
     };
 }
 
@@ -148,6 +158,7 @@ macro_rules! needed_use_declarations {
         use winapi::shared::minwindef::*;
         use winapi::shared::ntdef::*;
         use simplelog::{WriteLogger, LevelFilter, Config};
+        use std::sync::Mutex;
 
         use log::{debug, error, info};
         use $crate::xwf::*;
