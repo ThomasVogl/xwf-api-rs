28db65a5a6b53df02071e660d564eaec5821c710
Thomas Vogl <thomas.vogl@respon.se>
2025-01-22 17:33:16 +0100

    - improve SrcInfo::from_buffer method - remove empty, not used file

--- xwf_api_rs/src/xwf_types/implementations/impl_serde.rs
+++
@@ -1,2 +0,0 @@
-use serde::{Deserialize, Deserializer};
-use crate::xwf_types::FileTypeCategory;
--- xwf_api_rs/src/xwf_types/implementations/mod.rs
+++ xwf_api_rs/src/xwf_types/implementations/mod.rs
@@ -7,7 +7,6 @@ mod impl_conversions;
 mod impl_defaults;
 mod impl_displays;
 mod impl_orderings;
-mod impl_serde;
 
 impl ItemInfoDeletion {
     pub fn is_existing(&self) -> bool {
@@ -32,6 +31,14 @@ impl XwfDateTime {
 
 impl SrcInfo {
     pub fn from_buffer(data: &mut [u8]) -> SrcInfo {
+        assert!(!data.is_empty(), "Buffer cannot be empty");
+
+        #[cfg(not(feature = "api_21_1"))]
+        assert!(
+            data.len() as u64 <= 0x7FFF_FFFF_FFFF_FFFF,
+            "Buffer size exceeds maximum supported size for SrcInfo"
+        );
+
         SrcInfo {
             n_struct_size: size_of::<SrcInfo>() as u32,
             n_buf_size: data.len() as __int64,
